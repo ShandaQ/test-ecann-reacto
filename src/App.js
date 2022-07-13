f88c46ec3fc7ccc0c08c79d7e2397f670ec0ab4a
Erika Cannon <cannone.rva@gmail.com>
2022-07-13 15:43:20 -0400

    creating code for About and Nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import './App.css';
-import About from './components/About';
-
+import React, { useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Nav from "./components/Nav";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Project from "./components/Project";
+import Resume from "./components/Resume";
 
 function App() {
- 
+  const [categories] = useState([
+    {
+      name: "About Me",
+      // description:
+      //   "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "Projects", description: "Projects I've developed" },
+    { name: "Contact" },
+  
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <section className="my-5">
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        // contactSelected={contactSelected}
+        // setContactSelected={setContactSelected}
+      ></Nav>
       <main>
         <About></About>
       </main>
-      </section>
+    </section>
   );
 }
 
