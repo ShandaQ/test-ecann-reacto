e7c3667ea6c8db92f3960436254d14e09c635a8d
Erika Cannon <cannone.rva@gmail.com>
2022-07-17 14:14:08 -0400

    add resume component and pdf

---
+++ src/components/Resume/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.css';
+import { Container } from '@mui/material';
+import resume from '../../assets/';
+
+function Resume() {
+  return (
+    <div>
+      <h1>howdy!</h1>
+
+      <Container id='project-card'>
+        <p>Welcome!</p>
+        <div>
+          <a key='resume' target='_blank' rel='noreferrer' href={resume}>
+            view my resume
+          </a>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default Resume;
