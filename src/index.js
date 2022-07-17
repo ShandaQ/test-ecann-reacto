4cecbe90f2e80e88294f2a6d10299e740d0403b1
Erika Cannon <cannone.rva@gmail.com>
2022-07-17 12:56:01 -0400

    update index.js in src

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
+ReactDOM.render(
+  <BrowserRouter>
+    {" "}
+    <App />{" "}
+  </BrowserRouter>,
+  document.getElementById("root")
+); 
 
-const rootNode = document.getElementById('root');
-ReactDOM.render(<App />, rootNode);
 
 reportWebVitals();
