eec0afacc1a608416e4c7b1d6c1ccfc7c6847108
Erika Cannon <cannone.rva@gmail.com>
2022-07-17 13:33:50 -0400

    update contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,63 +1,86 @@
 import React, { useState } from 'react';
+import { Button, Container, TextField, SvgIcon, Tooltip } from '@mui/material';
+import './style.css';
+import EmailIcon from '@mui/icons-material/Email';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 
-import { validateEmail } from '../../utils/helpers';
-
-function ContactForm() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      setFormState({ [e.target.name]: e.target.value });
-      console.log('Form', formState);
-    }
-  };
-
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
-    }
-  };
+function Contact() {
+  const [name, setName, email, setEmail, message, setMessage] = useState('');
 
   return (
-    <section>
-      <h1 data-testid="h1tag">Contact me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button data-testid="button" type="submit">Submit</button>
+    <div>
+
+        <h1> let's connect </h1>
+
+
+      <Container>
+        <a
+          href='mailto:sraewolfskill@gmail.com'
+          rel='noreferrer'
+          target='_blank'
+        >
+          <Tooltip title='Email directly'>
+            <SvgIcon alt='email-icon' fontSize='large' component={EmailIcon} />
+          </Tooltip>
+        </a>{' '}
+        {/*  */}
+        <a
+          href='https://discordapp.com/users/746823093468790785'
+          rel='noreferrer'
+          target='_blank'
+        >
+          <Tooltip title='Add me on Discord'>
+            <SvgIcon
+              alt='discord-icon'
+              fontSize='large'
+              component={SportsEsportsIcon}
+            />
+          </Tooltip>
+        </a>
+      </Container>
+
+      <form>
+        <Container>
+          <TextField
+            id='form-input'
+            variant='filled'
+            placeholder='your name'
+            name='name'
+            type='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Container>
+
+        <Container>
+          <TextField
+            id='form-input'
+            variant='filled'
+            placeholder='your email'
+            name='email'
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Container>
+
+        <Container>
+          <TextField
+            id='textbox'
+            variant='filled'
+            placeholder='leave a message'
+            name='message'
+            type='message'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </Container>
+        <Button id='submit-button' type='submit'>
+          send
+        </Button>
       </form>
-    </section>
+    </div>
   );
 }
 
-export default ContactForm;
+export default Contact;
